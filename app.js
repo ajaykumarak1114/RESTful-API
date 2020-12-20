eec2100da4253aa6d8745d94d41d6f444fd5b1bb
Ajay <ajaykumarak1114@gmail.com>
2020-12-21 01:31:19 +0530

    Add put, patch and delete routes

--- app.js
+++ app.js
@@ -68,6 +68,40 @@ app.route("/articles/:articleTitle")
       res.send("Not found any article with the given title");
     }
   })
+})
+
+.put(function(req, res){
+  Article.update(
+    {  title : req.params.articleTitle},
+    { title : req.body.title, content : req.body.content},
+    { overwrite : true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the article");
+      }
+    }
+  )
+})
+
+.patch(function(req, res){
+  Article.updateOne(
+    { title : req.params.articleTitle},
+    { $set :  req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the article");
+      }
+    }
+
+  )
+})
+
+.delete(function(req, res){
+  Article.deleteOne({ title : req.params.articleTitle}, function(err){
+    if(!err){
+      res.send("Successfully deleted the article");
+    }
+  })
 });
 
 app.listen(3000, function(){
